4e24e039deb0bbca09093703f8bbd28e374f1b75
Brandon <Apex2.0predator@gmail.com>
2025-12-09 13:57:52 +0200

    added cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // relative paths for Netlify
   server: {
     proxy: {
-      '/api': {
-        target: 'https://shopsync-qx6o.onrender.com',
-        changeOrigin: true
-      }
+      '/api': 'https://shopsync-qx6o.onrender.com'
     }
   },
   build: {
-    outDir: 'build', // so Netlify can use frontend/build
-    emptyOutDir: true
+    outDir: 'dist',
   }
 })
+
+//vercel --prod
